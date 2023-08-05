e2f0d84ce1b2e39d07935b467bb9c6c9f7e9e699
Sebastian <120439871+SebastianL2@users.noreply.github.com>
2023-08-05 01:39:12 -0500

    Update scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -1,5 +1,5 @@
-
-const API_URL ='https://api-market-bivz.vercel.app/'
+//en caso de que falle alguna https://api-market-bivz-red.vercel.app https://api-market-bivz.vercel.app
+const API_URL ='https://api-market-bivz-red.vercel.app'
 let products=[];
 let listHTML="";
 
